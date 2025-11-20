a9ec5d49101b7e5372de3614f4a421f4c369d80a
John F Morton <john@johnfmorton.com>
2025-11-20 11:38:04 -0500

    feat(components): Add hyperlinks to footer and header components
    - Update footer component to include clickable SuperGeekery.com link with descriptive title attribute
    - Add descriptive text and hyperlink to GitHub repository in header component
    - Improve user experience by making external references clickable and properly formatted
    - Enhance component accessibility with target="_blank" and underline styling for links

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="text-center pb-6 text-slate-500 text-sm">
-   Created by John F Morton. 2025.
+   Created by John F Morton • <a href="https://supergeekery.com" title="A blog by John F Morton of interest only to nerds." class="underline" target="_blank">SuperGeekery.com</a>
 </footer>
